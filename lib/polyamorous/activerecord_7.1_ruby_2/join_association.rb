2960252c6d47eff9010b13e6d87fa4ece579cf55
Ankur Goel <ankurgoel@Ankurs-MacBook-Pro.local>
2024-06-27 18:05:32 +0530

    fix reflector issue

--- lib/polyamorous/activerecord_7.1_ruby_2/join_association.rb
+++ lib/polyamorous/activerecord_7.1_ruby_2/join_association.rb
@@ -1,39 +1,63 @@
-# active_record_5.1_ruby_2/join_association.rb
+require 'polyamorous/version'
 
-module Polyamorous
-  module JoinAssociationExtensions
-    include SwappingReflectionClass
-    def self.prepended(base)
-      base.class_eval { attr_reader :join_type }
+if ActiveRecord::VERSION::MAJOR >= 7
+  require "polyamorous/activerecord_7.1_ruby_3/reflection"
+else
+  require "polyamorous/activerecord_6.x_ruby_2/reflection"
+end
+
+if defined?(::ActiveRecord)
+  module Polyamorous
+    if defined?(Arel::InnerJoin)
+      InnerJoin = Arel::InnerJoin
+      OuterJoin = Arel::OuterJoin
+    else
+      InnerJoin = Arel::Nodes::InnerJoin
+      OuterJoin = Arel::Nodes::OuterJoin
     end
 
-    def initialize(reflection, children, polymorphic_class = nil,
-                   join_type = Arel::Nodes::InnerJoin)
-      @join_type = join_type
-      if polymorphic_class && ::ActiveRecord::Base > polymorphic_class
-        swapping_reflection_klass(reflection, polymorphic_class) do |reflection|
-          super(reflection, children)
-          self.reflection.options[:polymorphic] = true
-        end
-      else
-        super(reflection, children)
-      end
+    if defined?(::ActiveRecord::Associations::JoinDependency)
+      JoinDependency  = ::ActiveRecord::Associations::JoinDependency
+      JoinAssociation = ::ActiveRecord::Associations::JoinDependency::JoinAssociation
+      JoinBase = ::ActiveRecord::Associations::JoinDependency::JoinBase
+    else
+      JoinDependency  = ::ActiveRecord::Associations::ClassMethods::JoinDependency
+      JoinAssociation = ::ActiveRecord::Associations::ClassMethods::JoinDependency::JoinAssociation
+      JoinBase = ::ActiveRecord::Associations::ClassMethods::JoinDependency::JoinBase
+    end
+  end
+
+  require 'polyamorous/tree_node'
+  require 'polyamorous/join'
+  require 'polyamorous/swapping_reflection_class'
+
+  ar_version = ::ActiveRecord::VERSION::STRING[0,3]
+  ar_version = '3_and_4.0' if ar_version < '4.1'
+
+  method, ruby_version =
+    if RUBY_VERSION >= '2.0' && ar_version >= '4.1'
+      # Ruby 2; we can use `prepend` to patch Active Record cleanly.
+      [:prepend, '2']
+    else
+      # Ruby 1.9; we must use `alias_method` to patch Active Record.
+      [:include, '1.9']
     end
 
-    # Reference: https://github.com/rails/rails/commit/9b15db5
-    # NOTE: Not sure we still need it?
-    #
-    def ==(other)
-      base_klass == other.base_klass
-    end
+  %w(join_association join_dependency).each do |file|
+    require "polyamorous/activerecord_#{ar_version}_ruby_#{ruby_version}/#{file}"
+  end
 
-    def build_constraint(klass, table, key, foreign_table, foreign_key)
-      if reflection.polymorphic?
-        super(klass, table, key, foreign_table, foreign_key)
-          .and(foreign_table[reflection.foreign_type].eq(reflection.klass.name))
-      else
-        super(klass, table, key, foreign_table, foreign_key)
-      end
+  Polyamorous::JoinDependency.send(method, Polyamorous::JoinDependencyExtensions)
+  if method == :prepend
+    Polyamorous::JoinDependency.singleton_class
+    .send(:prepend, Polyamorous::JoinDependencyExtensions::ClassMethods)
+  end
+  Polyamorous::JoinAssociation.send(method, Polyamorous::JoinAssociationExtensions)
+
+  Polyamorous::JoinBase.class_eval do
+    if method_defined?(:active_record)
+      alias_method :base_klass, :active_record
     end
   end
 end
+
